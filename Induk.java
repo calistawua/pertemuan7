c3c22c28e8a0da3ec876e43412f4dfe3744546a1
calistawua <127809356+calistawua@users.noreply.github.com>
2024-05-06 11:44:04 +0800

    Add files via upload

---
+++ Induk.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan7;
+
+/**
+ *
+ * @author calista2201010294
+ * Tgl:2024-05-06
+ */
+public class Induk {
+    private String nama;
+    public int nilai=0;
+    
+    public void setNAMA(String nVAL){
+        this.nama = nVAL;
+    }
+     public void setNAMA(){
+         this.nama = "Siapa saya?";
+    }  
+     public String getNAMA(){
+         return this.nama;
+    }
+}
---
+++ Pertemuan7.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan7;
+
+/**
+ *
+ * @author calista2201010294
+ * Tgl:2024-05-06
+ */
+public class Pertemuan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Induk al = new Induk();
+        
+        System.out.print("1. Isi dari veriabel nama "+al.getNAMA());
+        al.setNAMA("I Wayan Made Putu Ketut Nyoam");
+        System.out.print("2. Isi dari veriabel nama "+al.getNAMA());
+        
+        al.setNAMA();
+        System.out.print("3. Isi dari veriabel nama "+al.getNAMA());
+        
+        }
+    }
+    
+
